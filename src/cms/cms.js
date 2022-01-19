2b804eef31c4b9552b3dd85798211c6e7f3745a0
Antony Briggs <antony.briggs@nationwide.co.uk>
2022-01-19 15:24:57 +0000

    github be

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -14,3 +14,22 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+const config = {
+    backend: {
+        name: github,
+        repo: piratebriggs/gatsby-starter-netlify-cms,
+      squash_merges: true,
+      commit_messages: {
+        create: 'docs(nel-site): Create {{collection}} "{{slug}}"',
+        update: 'docs(nel-site): Update {{collection}} "{{slug}}"',
+        delete: 'docs(nel-site): Delete {{collection}} "{{slug}}"',
+        uploadMedia: 'docs(nel-site): Upload "{{path}}"',
+        deleteMedia: 'docs(nel-site): Delete "{{path}}"',
+      },
+    },
+  
+    load_config_file: false,
+  };
+
+CMS.init({ config });
