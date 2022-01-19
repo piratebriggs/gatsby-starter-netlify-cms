adee230d7c17bee45e51c318d50504b896b514e2
Antony Briggs <antony.briggs@nationwide.co.uk>
2022-01-19 16:14:54 +0000

    move config to yml

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -15,25 +15,3 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 
-const config = {
-    backend: {
-        name: github,
-        repo: 'piratebriggs/gatsby-starter-netlify-cms',
-      squash_merges: true,
-      commit_messages: {
-        create: 'docs(nel-site): Create {{collection}} "{{slug}}"',
-        update: 'docs(nel-site): Update {{collection}} "{{slug}}"',
-        delete: 'docs(nel-site): Delete {{collection}} "{{slug}}"',
-        uploadMedia: 'docs(nel-site): Upload "{{path}}"',
-        deleteMedia: 'docs(nel-site): Delete "{{path}}"',
-      },
-      identity_url: "https://romantic-goodall-a60fe4.netlify.app/.netlify/identity",  // https://github.com/hfte/netlify-cms-with-selfhosted-gotrue-and-git-gateway
-      gateway_url:  "https://romantic-goodall-a60fe4.netlify.app/.netlify/git",
-      site_domain:  "https://romantic-goodall-a60fe4.netlify.app/",
-  
-    },
-  
-    load_config_file: false,
-  };
-
-CMS.init({ config });
